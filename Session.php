0baa8c21605e231e3cc0c6db4b69debc4ea555c7
Fabio Souto <fabiolimasouto@gmail.com>
2016-10-10 22:04:43 -0300

    Fix bug in session class

--- Session.php
+++ Session.php
@@ -7,16 +7,33 @@ class Session extends SymfonySession{
 
 	protected $namespace = '';
 
-	function setNamespace(){
-
+	function setNamespace($namespace){
+		$this->namespace = $namespace;
+		return $this;
 	}
 
 	function getNamespace(){
-
+		return $this->namespace;
 	}
 
-	function getArray($key, $default=[]){
-		return new \ArrayObject;
+	/**
+	 * Gets a stored value as ArrayObject instance
+	 * You don't need to set the value back after altering
+	 * the array through the ArrayObject API
+	 *
+	 * @param $key string
+	 * @param $default array
+	 * @return \ArrayObject
+	 */
+	function getArray($key, array $default=[]){
+		$array = $this->get($key, $default);
+		if(!is_array($array) && !$array instanceof \ArrayObject){
+			throw new \UnexpectedValueException("Session key '$key' should contain an array or ArrayObject instance.");
+		}
+		$arrayObject = is_array($array) ? new \ArrayObject($array) : $array;
+		$this->set($key, $arrayObject);
+		return $arrayObject;
+
 	}
 
 }
