5d3cf670d6f4893284b567ac5f6b256c5f7e04f0
Fabio Souto <fabiolimasouto@gmail.com>
2016-10-10 22:13:18 -0300

    Finish implementing and testing the getArray functionality

--- example.php
+++ example.php
@@ -7,4 +7,18 @@ use Obstart\EnhancedSymfonySession\Session;
 $session = new Session();
 $arrayObject = $session->getArray('search_history');
 
-dump($arrayObject);
+// First request: add some content
+//$arrayObject->append("search term 1");
+//$arrayObject->append("search term 2");
+
+// no need to set back
+
+// Second request: iteraate values previously set
+foreach($arrayObject as $item){
+	echo $item.'<br/>';
+}
+
+// Third request: clear array
+$arrayObject->exchangeArray([]);
+
+//dump($arrayObject);
